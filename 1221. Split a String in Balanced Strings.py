b3816d5e7909e806c066d03ab68a5c48f6596894
Liam <terroridk@gmail.com>
2020-10-22 19:21:54 -0400

    +

---
+++ 1221. Split a String in Balanced Strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        # each time we encounter a balanced string we want it
+        ans = x = 0
+        for i in s:
+            if i == 'L':
+                x += 1
+            else:
+                x -= 1
+            if not x:
+                ans += 1
+        return ans
---
+++ 1291. Sequential Digits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        ans = []
+        for i in range(1, 10):
+            x = i
+            for j in range(x+1, 10):
+                x = (x*10)+j
+                if self.check(x, low, high):
+                    ans.append(x)
+        return sorted(ans)
+
+    def check(self, n, l, h):
+        return l <= n <= h
